a4e3f723b2fc8648039bf90aa2000a5c8632b158
Alex <alexandru.gatu@gmail.com>
2021-12-06 10:04:38 +0200

    Add project files.

---
+++ BT_AUTO_2021_Programming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT_AUTO_2021_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
