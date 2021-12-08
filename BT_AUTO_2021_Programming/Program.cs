9262531efed33764f8a5b6eff0d4d5aa3543bfa3
Alex <alexandru.gatu@gmail.com>
2021-12-08 10:40:43 +0200

    update course 08.12.2021

--- BT_AUTO_2021_Programming/Program.cs
+++ BT_AUTO_2021_Programming/Program.cs
@@ -6,11 +6,52 @@ namespace BT_AUTO_2021_Programming
     {
         static void Main(string[] args)
         {
+            const int MAX_SIZE = 100;
+            const double PI = 3.141592;
+            
             Console.WriteLine("Hello World!");
             Console.WriteLine("Ana are mere");
             Console.WriteLine(6 + 2);
             Console.WriteLine("6 + 2");
-            
+
+            int numberOfStudents = 5;
+            int count;
+            int a, b;
+            a = numberOfStudents + 1;
+            a = a + numberOfStudents;
+            short s = 5;
+            ulong ul = 65;
+
+            float numberFloat = 5.6F;
+            double numberDouble = 5.6;
+
+            float f = (float)numberDouble;
+            int numberInt = (int)f;
+            long l = numberOfStudents;
+
+            Console.WriteLine(typeof(int).IsPrimitive);
+
+            bool myBoolean = false;
+
+            char ch = 'd'; // '1' != 1
+
+            string myString = "Ana are mere si pere";
+            string result = myString + " si struguri";
+            Console.WriteLine(result);
+
+            DateTime dataCurenta = new DateTime(2021, 12, 8);
+            Console.WriteLine(dataCurenta);
+
+            Console.WriteLine(numberFloat * a);
+            Console.WriteLine(5 / 6.0);
+            Console.WriteLine(5 % 6);
+
+            int x = 2;
+            x += 5; // x = x + 5;
+            // x++ <=> x = x + 1
+            // ++x <=> x = x + 1
+            Console.WriteLine(++x);
+            Console.WriteLine((x < a));
         }
     }
 }
