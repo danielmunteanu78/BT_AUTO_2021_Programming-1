0db8bf8ee9b736e0b26a1d2bcb3cdcde8a8f0e40
Alex <alexandru.gatu@gmail.com>
2022-01-12 09:50:59 +0200

    Adding Caluclator class

---
+++ NUnit_Auto_2022/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit_Auto_2022
+{
+    class Calculator : ICalculator
+    {
+        double a, b;
+        char op;
+
+        public Calculator(double a, double b, char op)
+        {
+            this.a = a;
+            this.b = b;
+            this.op = op;
+        }
+
+        public double Compute(double a, char op, double b)
+        {
+            switch(op)
+            {
+                case '+':
+                    {
+                        return a + b;
+                    }
+                case '-':
+                    {
+                        return a - b;
+                    }
+                case '*':
+                    {
+                        return a * b;
+                    }
+                case '/':
+                    {
+                        if( b == 0)
+                        {
+                            throw new ArgumentException();
+                        }
+                        return a / b;
+                    }
+                case '%':
+                    {
+                        if (b == 0)
+                        {
+                            throw new ArgumentException();
+                        }
+                        return a % b;
+                    }
+                default:
+                    {
+                        throw new ArgumentException();
+                    }
+            }
+        }
+    }
+}
