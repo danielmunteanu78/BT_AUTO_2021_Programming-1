d266d4a743f4e2105d6bfbed6716dc4bd3cdbd1e
Alex <alexandru.gatu@gmail.com>
2022-01-17 10:34:21 +0200

    Webelements locators/selectors

--- NUnit_Auto_2022/SeleniumTests.cs
+++ NUnit_Auto_2022/SeleniumTests.cs
@@ -13,21 +13,35 @@ namespace NUnit_Auto_2022
     {
 
         IWebDriver driver;
+        const string protocol = "https";
+        const string hostname = "magazinulcolectionarului.ro";
+        const string path = "/";
+
+        string url = protocol + "://" + hostname + path;
 
         [SetUp]
         public void Setup()
         {
-            //driver = new ChromeDriver();
+            driver = new ChromeDriver();
             //driver = new FirefoxDriver();
-            driver = new EdgeDriver();
+            //driver = new EdgeDriver();
+            
         }
 
         [Test]
         public void Test01()
         {    
            
-            driver.Url = "https://google.com";
-            driver.Navigate();            
+            driver.Url = url;
+            driver.Navigate();
+
+            //driver.Navigate().GoToUrl(url);
+
+            driver.Navigate().Back();
+
+            driver.Navigate().Forward();
+
+            driver.Navigate().Refresh();
         }
 
         [Test]
@@ -36,6 +50,26 @@ namespace NUnit_Auto_2022
             driver.Url = "https://learn.digitalstack.ro";
             driver.Navigate();
 
+        }
+
+        [Test]
+        public void Test03()
+        {
+            driver.Url = url;
+            driver.Navigate();
+
+            IWebElement body = driver.FindElement(By.ClassName("modal-header"));
+            body.Click();
+
+            IWebElement eaglemoss = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div[2]/nav/div/ul/li[1]/a/span[2]"));
+            eaglemoss.Click();
+
+            var addToCart = driver.FindElements(By.XPath("//*[@id='amasty-shopby-product-list']/div[2]/ol/li"));
+            foreach(IWebElement el in addToCart)
+            {
+                var text = el.FindElement(By.XPath("//span"));
+                Console.WriteLine(text.Text);
+            }
         }
 
         [TearDown]
