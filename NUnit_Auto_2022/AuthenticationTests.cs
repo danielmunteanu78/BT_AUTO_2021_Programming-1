55f541c3150090b07113b7d65cdaa84ca4e2d3ba
Alex <alexandru.gatu@gmail.com>
2022-01-19 10:33:18 +0200

    Full login test, implicit, explicit, fluent, sleep waits

--- NUnit_Auto_2022/AuthenticationTests.cs
+++ NUnit_Auto_2022/AuthenticationTests.cs
@@ -1,9 +1,11 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace NUnit_Auto_2022
 {
@@ -15,7 +17,7 @@ namespace NUnit_Auto_2022
         const string protocol = "http";
         const string hostname = "86.121.249.150";
         const string port = "4999";
-        const string path = "/#/login";
+        const string path = "/#/";
 
         string url = protocol + "://" + hostname + ":" + port + path;
 
@@ -25,18 +27,31 @@ namespace NUnit_Auto_2022
             driver = new ChromeDriver();
         }
 
-        [TestCase("dinosaur", "dinosaurpassword")]
-        [TestCase("dinosaur", "")]
-        [TestCase("", "")]
-        [TestCase("", "dinopass")]
-        public void Test01(string user, string pass)
+        [TestCase("dinosaur", "dinosaurpassword", "", "")]
+        [TestCase("dinosaur", "", "", "Password is required!")]
+        [TestCase("", "", "Username is required!", "Password is required!")]
+        [TestCase("", "dinopass", "Username is required!", "")]
+        public void Test01(string user, string pass, string userErr, string passErr)
         {
             Console.WriteLine(url);
             driver.Navigate().GoToUrl(url);
 
+            var pageText = driver.FindElement(By.CssSelector("#root > div > div.content > div > div:nth-child(1) > div > div > h1 > small"));
+            Assert.AreEqual("Home", pageText.Text);
+
+            var loginLink = driver.FindElement(By.CssSelector("#root > div > div.sidebar > a:nth-child(2)"));
+            loginLink.Click();
+            Console.WriteLine(loginLink.GetAttribute("href"));
+
+            pageText = driver.FindElement(By.CssSelector("#root > div > div.content > div > div:nth-child(1) > div > div > h1 > small"));
+            Assert.AreEqual("Authentication", pageText.Text);
+
             var username = driver.FindElement(By.Id("input-login-username"));
             var password = driver.FindElement(By.Id("input-login-password"));
             var submit = driver.FindElement(By.CssSelector("#login-form > div:nth-child(3) > div.text-left.col-lg > button"));
+
+            var usernameError = driver.FindElement(By.CssSelector("#login-form > div:nth-child(1) > div > div > div.text-left.invalid-feedback"));
+            var passwordError = driver.FindElement(By.CssSelector("#login-form > div.form-group.row.row-cols-lg-true > div > div > div.text-left.invalid-feedback"));
 
             username.Clear();
             username.SendKeys(user);
@@ -46,6 +61,60 @@ namespace NUnit_Auto_2022
 
             submit.Submit();
 
+            Assert.AreEqual(userErr, usernameError.Text);
+            Assert.AreEqual(passErr, passwordError.Text);
+
+        }
+
+
+        // implicit wait test
+        [Test]
+        public void Test02()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            string url = protocol + "://" + hostname + ":8081/lazy.html";
+            driver.Navigate().GoToUrl(url);
+
+            var button1 = driver.FindElement(By.Id("btn1"));
+            button1.Click();
+
+        }
+
+        // explicit wait test
+        [Test]
+        public void Test03()
+        {
+            string url = protocol + "://" + hostname + ":8081/lazy.html";
+            driver.Navigate().GoToUrl(url);
+
+            var button1 = Utils.WaitForElement(driver, 20, By.Id("btn1"));
+            button1.Click();
+
+        }
+
+        [Test]
+        public void Test04()
+        {
+            string url = protocol + "://" + hostname + ":8081/lazy.html";
+            driver.Navigate().GoToUrl(url);
+
+            var button2 = Utils.WaitForElement(driver, 15, By.Id("btn2"));
+            for(int i =0; i < 100; i ++)
+            {
+                button2.Click();
+            }
+            Thread.Sleep(20000); // pausing test execution for 20 seconds !!! Please avoid !!
+        }
+
+        [Test]
+        public void Test05()
+        {
+            string url = protocol + "://" + hostname + ":8081/lazy.html";
+            driver.Navigate().GoToUrl(url);
+
+            var element = Utils.WaitForFluentElement(driver, 20, By.Id("btn2"));
+            element.Click();
         }
 
         [TearDown]
---
+++ NUnit_Auto_2022/Utils.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit_Auto_2022
+{
+    public class Utils
+    {
+
+        public static IWebElement WaitForElement(IWebDriver driver, int seconds, By locator)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        public static IWebElement WaitForFluentElement(IWebDriver driver, int seconds, By locator)
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver)
+            {
+                Timeout = TimeSpan.FromSeconds(seconds),
+                PollingInterval = TimeSpan.FromMilliseconds(100),
+                Message = "Sorry !! The element in the page cannot be found!"
+            };
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            return fluentWait.Until(x => x.FindElement(locator));
+        }
+
+    }
+}
