6e21471e563dca0166185cae691fcb1fc32f55c0
Alex <alexandru.gatu@gmail.com>
2022-01-28 10:34:36 +0200

    PageFactory + Ecom site example

---
+++ NUnit_Auto_2022/PageModels/PageFactory/LoginPage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit_Auto_2022.PageModels.PageFactory
+{
+    public class LoginPage
+    {
+        IWebDriver driver;
+        // Page elements that are found automatically once the class is instantiated and page is loaded
+        private IWebElement authPageTextElem => driver.FindElement(By.ClassName("text-muted"));
+        private IWebElement usernameLabelElem => driver.FindElement(By.CssSelector("#login-form > div:nth-child(1) > label"));
+        private IWebElement usernameInputElem => driver.FindElement(By.Id("input-login-username"));
+        private IWebElement usernameErrElem => driver.FindElement(By.CssSelector("#login-form > div:nth-child(1) > div > div > div.text-left.invalid-feedback"));
+        private IWebElement passwordLabelElem => driver.FindElement(By.CssSelector("#login-form > div.form-group.row.row-cols-lg-true > label"));
+        private IWebElement passwordInputElem => driver.FindElement(By.Id("input-login-password"));
+        private IWebElement passwordErrElem => driver.FindElement(By.CssSelector("#login-form > div.form-group.row.row-cols-lg-true > div > div > div.text-left.invalid-feedback"));
+        private IWebElement submitButtonElem => driver.FindElement(By.ClassName("btn-primary"));
+
+        // Login page constructor that initializes the driver
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // Returns the text that appears on the AUTH page to be checked in the test
+        public string CheckPage()
+        {
+            return authPageTextElem.Text;
+        }
+
+        // Login page with username and password passed from the test
+        public void Login(string user, string pass)
+        {
+            usernameInputElem.Clear();
+            usernameInputElem.SendKeys(user);
+            passwordInputElem.Clear();
+            passwordInputElem.SendKeys(pass);
+            submitButtonElem.Submit();
+        }
+
+    }
+}
--- NUnit_Auto_2022/Tests/AuthTest.cs
+++ NUnit_Auto_2022/Tests/AuthTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using NUnit_Auto_2022.PageModels.POM;
+using NUnit_Auto_2022.PageModels.PageFactory;
 using NUnit_Auto_2022.Utilities;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -21,11 +22,24 @@ namespace NUnit_Auto_2022.Tests
             driver = new ChromeDriver();
         }
 
+        // Test auth with Page Object model
         [Test]
         public void BasicAuth()
         {
             driver.Navigate().GoToUrl(url + "login");
-            LoginPage lp = new LoginPage(driver);
+            // This is beacuse we have 2 classes named LoginPage one on POM other on PageFactory
+            // In real life framework you will have just one type of desigm pattern (POM/PF)
+            PageModels.POM.LoginPage lp = new PageModels.POM.LoginPage(driver);
+            Assert.AreEqual("Authentication", lp.CheckPage());
+            lp.Login("user1", "pass1");
+        }
+
+        // Test auth with Page factory
+        [Test]
+        public void BasicAuthPf()
+        {
+            driver.Navigate().GoToUrl(url + "login");
+            PageModels.PageFactory.LoginPage lp = new PageModels.PageFactory.LoginPage(driver);
             Assert.AreEqual("Authentication", lp.CheckPage());
             lp.Login("user1", "pass1");
         }
